dfcb06cb1f7813ea44c4b2f0a6c57ee083195b80
rovlxn06 <31325728+rovlxn06@users.noreply.github.com>
2017-09-10 21:28:45 -0300

    Adições
    O programa agora mostra a mão e a sequencia de cartas de cada jogador.

--- poker_lp3/Carta.php
+++ poker_lp3/Carta.php
@@ -2,14 +2,75 @@
 
 class Carta {
 
-    //naipes: 1 (C), 2 (D), 3 (E), 4 (P));
+    //naipes: 1 (C), 2 (O), 3 (E), 4 (P));
     //valores: 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 (J), 12 (Q), 13 (K), 14 (A));
     public $naipe;
     public $valor;
+    public $naipe_real;
+    public $valor_real;
 
     public function Carta($n, $v)
     {
         $this->naipe = $n;
         $this->valor = $v;
+
+        switch($this->naipe)
+        {
+          case 1:
+            $this->naipe_real = "COPAS";
+            break;
+          case 2:
+            $this->naipe_real = "OUROS";
+            break;
+          case 3:
+            $this->naipe_real = "ESPADAS";
+            break;
+          case 4:
+            $this->naipe_real = "PAUS";
+            break;
+        }
+
+        switch($this->valor)
+        {
+          case 2:
+            $this->valor_real = "2";
+            break;
+          case 3:
+            $this->valor_real = "3";
+            break;
+          case 4:
+            $this->valor_real = "4";
+            break;
+          case 5:
+            $this->valor_real = "5";
+            break;
+          case 6:
+            $this->valor_real = "6";
+            break;
+          case 7:
+            $this->valor_real = "7";
+            break;
+          case 8:
+            $this->valor_real = "8";
+            break;
+          case 9:
+            $this->valor_real = "9";
+            break;
+          case 10:
+            $this->valor_real = "10";
+            break;
+          case 11:
+            $this->valor_real = "J";
+            break;
+          case 12:
+            $this->valor_real = "Q";
+            break;
+          case 13:
+            $this->valor_real = "K";
+            break;
+          case 13:
+            $this->valor_real = "A";
+            break;
+        }
     }
 }
